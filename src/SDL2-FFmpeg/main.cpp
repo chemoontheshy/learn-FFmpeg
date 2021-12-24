bceac1f16868392fdc2df9f9cd3910f3dadab31a
ZhenFei Xu <570635410@qq.com>
2021-12-24 18:08:41 +0800

    test

--- src/SDL2-FFmpeg/main.cpp
+++ src/SDL2-FFmpeg/main.cpp
@@ -16,44 +16,27 @@ typedef struct Address
 	uint16_t    Port;
 } Address;
 
+class Dog
+{
+public:
+	Dog(const int age) :m_iAge(age) {};
+
+private:
+	int m_iAge;
+
+};
+
 int main(int argc, char* argv[])
 {
-	if (argc < 5) {
-		std::cout << "params error.: H264 NONE NONE NONE 192.168.0.31 8002" << std::endl;
-		return -1;
+	std::vector<Dog*> lstDog;
+	lstDog.push_back(new Dog(1));
+	lstDog.push_back(new Dog(2));
+	lstDog.push_back(new Dog(3));
+	lstDog.push_back(new Dog(4));
+
+	for (auto& dog : lstDog) {
+		delete dog;
 	}
-	std::cout << argc << std::endl;
-	std::vector<CodecID> lstCodecID;
-	std::vector<Address> lstAddress;
-	Address address;
-	for (size_t i = 1; i < 5; i++) {
-		//std::cout << argv[i] << std::endl;
-		if (!strcmp(argv[i], "H264")) {
-			lstCodecID.push_back(CodecID::H264);
-		}
-		if (!strcmp(argv[i], "HEVC")) {
-			lstCodecID.push_back(CodecID::HEVC);
-		}
-	}
-	if (argc > 5) {
-		for (size_t i = 5; i < argc; i += 2) {
-			std::cout << argv[i] << std::endl;
-			address.IPorHost = argv[i];
-			address.Port = std::atoi(argv[i + 1]);
-			lstAddress.push_back(address);
-		}
-	}
-	for (auto& codecID : lstCodecID) {
-		if (codecID == CodecID::H264) {
-			std::cout << "CodecID::H264" << std::endl;
-		}
-		if (codecID == CodecID::HEVC) {
-			std::cout << "CodecID::HEVC" << std::endl;
-		}
-	}
-	for (auto& taddress : lstAddress) {
-		std::cout << taddress.IPorHost << std::endl;
-		std::cout << taddress.Port << std::endl;
-	}
+	
 	return 0;
 }
