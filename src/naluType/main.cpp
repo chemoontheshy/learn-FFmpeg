f937f2e9ade6d613ca94aa02fdd084438e067143
chemoontheshy <xzfandzgx@gmail.com>
2022-06-20 10:15:22 +0800

    add

--- src/naluType/main.cpp
+++ src/naluType/main.cpp
@@ -24,15 +24,33 @@ struct NaluHeader
 	uint8_t forbidden_zero_bit : 1;
 };
 
+static void __EBSP_TO_RBSP(vsnc::vnal::Nalu& nalu) noexcept
+{
+	size_t size = nalu.Length;
+	uint8_t* src = nalu.Data;
+	for (size_t start = 0; start < nalu.Length; start++) {
+		if (src[start] == 0 && src[start + 1] == 0 && src[start + 2] == 3 && src[start + 3] <= 3) {
+			--size;
+			size_t index = start + 2;
+			for (size_t j = index; j < nalu.Length - 1; j++) {
+				src[j] = src[j + 1];
+			}
+		}
+	}
+	nalu.Length = size;
+	std::cout << nalu.Length << std::endl;
+	//return size;
+}
+
 int main()
 {
-	vsnc::vnal::SplitNalu splitNalu("../../3rdparty/video/test.h264");
+	vsnc::vnal::SplitNalu splitNalu("../../3rdparty/video/1280x720_30fps_60s.h264");
 	size_t num = 0;
 	size_t len = 0;
 	size_t startcodeLen = 0;
 	while (true)
 	{
-		num++;
+		//num++;
 		auto nalu = splitNalu.GetNext();
 		if (nalu.Length <= 0) break;
 		//len += nalu.Length;
@@ -58,9 +76,29 @@ int main()
 
 		if (naluHeader)
 		{
+			if (naluHeader->nal_unit_type == 7)
+			{
+				//EBSP 67 64 00 1f ac d9 40 50 05 ba 10 00 00 03 00 10 00 00 03 03 c0 f1 83 19 60
+				//RBSP 67 64 00 1f ac d9 40 50 05 ba 10 00 00 00 10 00 00 03 c0 f1 83 19 60
+				//     67 64 00 1f ac d9 40 50 05 ba 10 00 00 00 10 00 00 03 c0 f1 83 19 60
+				uint8_t* EBSPData = new uint8_t[nalu.Length];
+				memcpy(EBSPData, nalu.Data + 4, nalu.Length - 4);
+				vsnc::vnal::Nalu EBSP{ EBSPData,nalu.Length - 4 };
+
+
+				__EBSP_TO_RBSP(EBSP);
+				
+				std::cout << "test" << std::endl;
+			}
+
+			if (naluHeader->nal_unit_type != 7 && naluHeader->nal_unit_type != 8 && naluHeader->nal_unit_type != 6)
+			{
+				num++;
+			}
 			//std::cout << "forbidden_zero_bit : " << static_cast<size_t>(naluHeader->forbidden_zero_bit) << std::endl;
 			//std::cout << "nal_ref_idc        : " << static_cast<size_t>(naluHeader->nal_ref_idc) << std::endl;
 			std::cout << num << "," << static_cast<size_t>(naluHeader->nal_unit_type) << "," << startcodeLen << "," << nalu.Length<< std::endl;
+			
 		}
 	}
 	//std::cout << len << std::endl;
